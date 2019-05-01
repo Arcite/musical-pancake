f5134a0d789e1e2fa64df5f2bb272605d03435c4
Arcite <kristopher.wagner@outlook.com>
2019-04-30 21:32:27 -0400

    Use numpy instead of random

--- dice_test.py
+++ dice_test.py
@@ -1,19 +1,18 @@
 import unittest
 import dice
+import numpy as np
 
 class TestDiceMethods(unittest.TestCase):
 
     def test_rollD20(self):
         d20 = dice.Die(20)
-        for _ in range(0,1000):
-            result = d20.roll()
-            self.assertTrue(result > 0 & result < 21)
+        results = d20.roll(10000)
+        self.assertTrue(np.all(results > 0) & np.all(results < 21))
     
     def test_rolD6(self):
         d6 = dice.Die(6)
-        for _ in range(0,1000):
-            result = d6.roll()
-            self.assertTrue(result > 0 & result < 7)
+        results = d6.roll(10000)
+        self.assertTrue(np.all(results > 0) & np.all(results < 6))
 
 if __name__ == '__main__':
     unittest.main()
