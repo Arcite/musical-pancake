2c61118a7cb65e85a383fdf0d72306b615410423
Arcite <kristopher.wagner@outlook.com>
2019-05-14 19:47:06 -0400

    Fix linting errors

--- die.py
+++ die.py
@@ -13,10 +13,10 @@ def roll(dice, times=1):
             times is the number of times to roll the dice combination
     FCTVAL === numpy array of times random numbers between [len(dice), sum(dice)]
     """
-    retVal = np.zeros((1, times), dtype=np.int32)
+    ret_val = np.zeros((1, times), dtype=np.int32)
     for die in dice:
-        retVal = retVal + np.random.randint(1, die, times)
-    return retVal
+        ret_val = ret_val + np.random.randint(1, die, times)
+    return ret_val
 
 def roll_with_modifier(dice, times=1, modifier=0):
     """
@@ -28,13 +28,13 @@ def roll_with_modifier(dice, times=1, modifier=0):
     """
     return roll(dice, times) + modifier
 
-def hit_or_miss(rolls, ac):
+def hit_or_miss(rolls, armor_class):
     """
     PRE:    rolls is a numpy array simulating dice rolls
-            ac is the minimum value to be considered a "hit"
+            armor_class is the minimum value to be considered a "hit"
     FCTVAL === numpy array of Boolean values where True is a hit and False is a miss
     """
-    return rolls >= ac
+    return rolls >= armor_class
 
 def calculate_damage(hits, dice, modifier=0):
     """
@@ -76,32 +76,31 @@ class TestDiceMethods(unittest.TestCase):
         """
         Uses pregenerated arrays to verify hit_or_miss
         """
-        t1 = np.array([1, 2, 3, 4, 5])
-        v1 = np.array([False, False, False, False, False])
-        self.assertTrue(np.all(hit_or_miss(t1, 6) == v1))
+        test_1 = np.array([1, 2, 3, 4, 5])
+        val_1 = np.array([False, False, False, False, False])
+        self.assertTrue(np.all(hit_or_miss(test_1, 6) == val_1))
 
-        t2 = np.array([6, 7, 8, 9, 10])
-        v2 = np.array([True, True, True, True, True])
-        self.assertTrue(np.all(hit_or_miss(t2, 5) == v2))
+        test_2 = np.array([6, 7, 8, 9, 10])
+        val_2 = np.array([True, True, True, True, True])
+        self.assertTrue(np.all(hit_or_miss(test_2, 5) == val_2))
 
-        t3 = np.array([11, 13, 15, 17, 19])
-        v3 = np.array([False, False, True, True, True])
-        self.assertTrue(np.all(hit_or_miss(t3, 15) == v3))
+        test_3 = np.array([11, 13, 15, 17, 19])
+        val_3 = np.array([False, False, True, True, True])
+        self.assertTrue(np.all(hit_or_miss(test_3, 15) == val_3))
 
     def test_calculate_damage(self):
         """
         Uses pregenerated arrays to verify calculate_damage
         """
-        t1 = np.array([False, False, False, False, False])
-        v1 = np.array([0, 0, 0, 0, 0])
-        self.assertTrue(np.all(calculate_damage(t1, [6]) == v1))
+        test_1 = np.array([False, False, False, False, False])
+        val_1 = np.array([0, 0, 0, 0, 0])
+        self.assertTrue(np.all(calculate_damage(test_1, [6]) == val_1))
 
-        t2 = np.array([True, True, True, True, True])
-        self.assertTrue(np.all(calculate_damage(t2, [6]) > 0))
+        test_2 = np.array([True, True, True, True, True])
+        self.assertTrue(np.all(calculate_damage(test_2, [6]) > 0))
 
         results = calculate_damage(hit_or_miss(roll([20], 10000), 10), [8])
         self.assertTrue(np.all(results >= 0) and np.all(results <= 8))
-
 
 if __name__ == '__main__':
     unittest.main()
